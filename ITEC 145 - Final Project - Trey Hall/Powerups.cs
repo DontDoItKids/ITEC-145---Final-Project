b2898c5ec20ebb166f513e656af6d210d6748e45
Trey Hall <Trey.Hall@mymhc.ca>
2023-04-04 18:08:56 -0600

    Update godamit

--- ITEC 145 - Final Project - Trey Hall/Powerups.cs
+++ ITEC 145 - Final Project - Trey Hall/Powerups.cs
@@ -8,5 +8,28 @@ namespace ITEC_145___Final_Project___Trey_Hall
 {
     internal class Powerups
     {
+
+
+        //Fields
+        private int _height;
+        private int _width;
+
+        private int _x;
+        private int _y;
+
+        private Brush _brush;
+
+        //Properties
+
+
+        //Constructor
+        public Powerups()
+        {
+
+        }
+
+        //Methods
+
+
     }
 }
